9e7434fe61b9017fc347e1f5b6cfefd3a13b6e52
hello02923 <55674807+hello02923@users.noreply.github.com>
2019-11-01 18:07:32 +0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,15 @@
+class tree:
+
+  def __init__(self, data):
+    self.left = None
+    self.right = None
+    self.root = None
+    self.data = data
+    
+    
+    
+  def PrintTree(self):
+  
+  
+  
+  def 
